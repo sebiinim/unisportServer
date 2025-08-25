73a5cbf7df17ec7d287e17c1a1dc784887b6676a
sebin <1219sebin@gmail.com>
2025-08-25 23:40:33 +0900

    배포가 하고 싶어요

--- src/main/java/com/example/unisportserver/data/entity/UserEntity.java
+++ src/main/java/com/example/unisportserver/data/entity/UserEntity.java
@@ -18,6 +18,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String loginId;
     private String password;
 
